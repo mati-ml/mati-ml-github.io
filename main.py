fb10d028972f6faca5ec7115c3c6cff3f4d9d6ac
mati-ml <mmullerlanas@gmail.com>
2023-10-02 19:48:02 -0300

    arreglado

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-import app
+
 import flask
 from flask import Flask, request, render_template
 import time
@@ -6,48 +6,35 @@ from pymongo import MongoClient
 import ssl
 
 # Configuración de MongoDB
-mongo_uri = "mongodb+srv://pagina:appuai@cluster0.sufar8c.mongodb.net/?retryWrites=true&w=majority"  # Reemplaza con tu URI de MongoDB
-client = MongoClient(mongo_uri,ssl=True,
-        ssl_cert_reqs=ssl.CERT_REQUIRED,
-        ssl_ca_certs="cacert.pem")
-db = client['sensores']
-collection = db['valores']
+# Resto del código...
+
 
 # Buscar un documento con el nombre "A001"
-documento = collection.find_one({"nombre": "A001"})
-
 # Verificar si se encontró un documento
-if documento:
-    # Obtener el valor del campo deseado, por ejemplo, 'valor'
-    x= documento.get('valor')
-print(x)
 
-
-
-app = Flask(__name__)    
+app = Flask(__name__)
 app.static_folder = 'static'
 
 @app.route('/')
 def inicio():
     while True:
         mongo_uri = "mongodb+srv://pagina:appuai@cluster0.sufar8c.mongodb.net/?retryWrites=true&w=majority"  # Reemplaza con tu URI de MongoDB
-        client = MongoClient(mongo_uri,ssl=True,
-                ssl_cert_reqs=ssl.CERT_REQUIRED,
-                ssl_ca_certs="cacert.pem")
+
+        # Crear una conexión MongoDB con SSL
+        client = MongoClient(mongo_uri)
         db = client['sensores']
         collection = db['valores']
 
-            # Buscar un documento con el nombre "A001"
+        # Buscar un documento con el nombre "A001"
         documento = collection.find_one({"nombre": "A001"})
 
-            # Verificar si se encontró un documento
+        # Verificar si se encontró un documento
         if documento:
-               # Obtener el valor del campo deseado, por ejemplo, 'valor'
-            x= documento.get('valor')
+            # Obtener el valor del campo deseado, por ejemplo, 'valor'
+            x = documento.get('valor')
             print(x)
             return render_template("/index.html", x=x)
         time.sleep(3)
 
-
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
